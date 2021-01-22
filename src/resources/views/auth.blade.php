022487f7425c2b41a0f1be7a26145afa955b73c3
Terobit1 <oleg1212142@gmail.com>
2021-01-22 17:25:58 +0200

    Add new files

---
+++ src/resources/views/auth.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Material Design for Bootstrap</title>
+        <!-- MDB icon -->
+        <link rel="icon" href="resources/img/mdb-favicon.ico" type="image/x-icon">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <!-- Bootstrap core CSS -->
+
+        <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+        <!-- Material Design Bootstrap -->
+
+        <link href="{{ URL::asset('css/mdb.min.css') }}" rel="stylesheet" type="text/css" >
+        <!-- Your custom styles (optional) -->
+
+        <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+    </head>
+    <body >
+    <div style="margin-top: 10%; margin-left: auto; margin-right: auto; width:450px;  " class="border p-4 d-flex text-center justify-content-center mb-5 flex-center align-items-center ">
+    
+            <form >
+            <!-- Email input -->
+            <label style="margin-bottom: 24px;" class="form-check-label" for="form2Example3"> Login </label>
+            <div class="form-outline mb-4">
+                <input type="email" id="form2Example1" class="form-control" placeholder="Email address" />
+                
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="form2Example2" class="form-control" placeholder="Password"/>
+                
+            </div>
+    
+            <!-- 2 column grid layout for inline styling -->
+            <div class="row mb-4">
+                <div class="col d-flex justify-content-center">
+                <!-- Checkbox -->
+                <div class="form-check" style="margin-top: 9px;">
+                    <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="form2Example3"
+                    checked
+                    />
+                    <label class="form-check-label" for="form2Example3"> Remember me </label>
+                </div>
+                </div>
+
+                <div class="col">
+                 <!-- Submit button -->
+                <button type="submit" class="btn btn-dark btn-block ">Sign in</button>
+                </div>
+            </div>
+
+            
+            
+
+            <!-- Register buttons -->
+            <div class="text-center  " >
+                
+                <button type="button" class="btn btn-primary " style="margin-left: auto; margin-right: auto; width:100%;">
+                <i class="fab fa-facebook-f"> Sign in facebook</i>
+                </button>
+
+                <button type="button" class="btn btn-info " style="margin-left: auto; margin-right: auto; width:100%;">
+                <i class="fab fa-google"> Sign in Google</i>
+                </button>
+
+                </button>
+            </div>
+            </form>
+    </div>
+    </body>
+        <!-- End your project here-->
+  <!-- jQuery -->
+  <script type="text/javascript" src="resources/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="../js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="../js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+
+</html>
---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design for Bootstrap</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+  
+</head>
+<body>
+ <header>
+   
+    <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light ">
+  <!-- Container wrapper -->
+    
+      <div class="container-fluid" style="
+      padding-left: 0px;>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active"><a href="#">Library</a></li>
+            
+          </ol>
+        
+      </div>
+      
+      <!-- Left links -->
+
+      <!-- Search form -->
+      <form class="d-flex input-group w-auto">
+        <input style="margin: auto; "
+          type="search"
+          class="form-control"
+          placeholder="Write"
+          aria-label="Search"
+        />
+        <button
+          style="margin-left: 0; padding: 1;  outline: none;"
+          class="btn-outline-primary"
+          type="button"
+          data-mdb-ripple-color="dark"
+        >
+          Search
+        </button>
+      </form>
+    
+      <div class="btn-group ">
+        <button
+          style="padding: 1;padding-top: 2.440;padding-top: 9px;padding-bottom: 9px;padding-right: 5px;"
+          type="button"
+          class="btn btn-light dropdown-toggle"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+        
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <li>
+            <hr class="dropdown-divider" />
+          </li>
+          <li><a class="dropdown-item" href="#">Separated link</a></li>
+        </ul>
+      </div>
+</nav>
+
+
+<!-- Navbar -->
+ </header>
+ 
+ <div class="d-flex " style='' >
+  <div class="p-2 border " style=' height: 900px; padding: 10px;'>
+    <h4 style='padding: 24px;'>Рік видання</h4>
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label class="form-check-label" for="flexCheckDefault">
+        1971-1980
+      </label>
+    </div>
+    
+    <!-- Checked checkbox -->
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckChecked"
+        checked
+      />
+      <label class="form-check-label" for="flexCheckChecked">
+        1981-1990
+      </label>
+    </div>
+
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label class="form-check-label" for="flexCheckDefault">
+        1991-2000
+      </label>
+    </div>
+    
+    <!-- Checked checkbox -->
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckChecked"
+        checked
+      />
+      <label class="form-check-label" for="flexCheckChecked">
+        2001-2010
+      </label>
+    </div>
+
+    <h4 style='padding: 24px;'>Видавництво</h4>
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label class="form-check-label" for="flexCheckDefault">
+        Default checkbox
+      </label>
+    </div>
+    
+    <!-- Checked checkbox -->
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckChecked"
+        checked
+      />
+      <label class="form-check-label" for="flexCheckChecked">
+        Checked checkbox
+      </label>
+    </div>
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckDefault"
+      />
+      <label class="form-check-label" for="flexCheckDefault">
+        Default checkbox
+      </label>
+    </div>
+    
+    <!-- Checked checkbox -->
+    <div style='padding-left: 23%; padding-bottom: 24px;' class="form-check">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        value=""
+        id="flexCheckChecked"
+        checked
+      />
+      <label class="form-check-label" for="flexCheckChecked">
+        Checked checkbox
+      </label>
+    </div>
+  
+  
+  
+  </div>
+  <div class="p-10 flex-fill" style=''>
+
+    <div class="card w-75" style="margin: 35px;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
+    </div>
+    
+    <div class="card w-75" style="margin: 35px;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
+    </div>
+
+
+
+  </div>
+</div>
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/popper.js"></script>
+ 
+  <script type="text/javascript" src="js/ bootstrap.bundle.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+
+
+
+</body>
+</html>
